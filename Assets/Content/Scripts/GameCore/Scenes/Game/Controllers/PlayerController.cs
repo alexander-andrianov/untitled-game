34ea22ae2dc53eb79c6d4b3fe1b557fcacdea080
alexander-andrianov-whitesharx <alexander.andrianov@whitesharx.com>
2023-01-03 14:05:39 +0300

    added base ghost logic

--- Assets/Content/Scripts/GameCore/Scenes/Game/Controllers/PlayerController.cs
+++ Assets/Content/Scripts/GameCore/Scenes/Game/Controllers/PlayerController.cs
@@ -1,34 +1,49 @@
+using System;
+using System.Threading.Tasks;
 using Cinemachine;
+using Cysharp.Threading.Tasks;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Playables;
 
 namespace Content.Scripts.GameCore.Scenes.Game.Controllers
 {
     public class PlayerController : NetworkBehaviour
     {
-        private const float SpeedValue = 350f;
+        private const float MovementSpeedValue = 7f;
+        private const float RotationSpeedValue = 350f;
         private const float RotationDumpingValue = 10f;
-        
-        [SerializeField]
-        private Light playerLight;
-        
-        public Light PlayerLight => playerLight;
-        
-        private PlayerInput playerInput;
-        private Rigidbody localRigidbody;
 
-        public override void OnNetworkSpawn() {
+        [SerializeField] private PlayableDirector ghostDirector;
+
+        [SerializeField] private Transform mainModel;
+
+        [SerializeField] private Light playerLight;
+
+        public Light PlayerLight => playerLight;
+
+        private PlayerInput playerInput;
+        private Transform localTransform;
+        private Vector3 currentMovementInput;
+
+        private PlayableDirector activeDirector;
+
+        public override void OnNetworkSpawn()
+        {
             if (!IsOwner)
             {
                 enabled = false;
             }
         }
 
-        private void Start()
+        private async void Start()
         {
             Initialize();
+
+            await Task.Delay(TimeSpan.FromSeconds(15f));
+            HandleKill();
         }
-        
+
         private void OnDisable()
         {
             if (IsOwner)
@@ -37,7 +52,7 @@ namespace Content.Scripts.GameCore.Scenes.Game.Controllers
             }
         }
 
-        private void FixedUpdate()
+        private void Update()
         {
             MovePlayer();
             RotatePlayer();
@@ -48,27 +63,43 @@ namespace Content.Scripts.GameCore.Scenes.Game.Controllers
             var virtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
             virtualCamera.Follow = transform;
 
-            localRigidbody = GetComponent<Rigidbody>();
-            
+            localTransform = GetComponent<Transform>();
+
             playerInput = new PlayerInput();
             playerInput.Enable();
         }
-        
-        private void MovePlayer()
-        {  
-            var movementInput = playerInput.Player.Move.ReadValue<Vector3>().normalized;
-            movementInput.y = 0;
 
-            localRigidbody.velocity = movementInput * (SpeedValue * Time.fixedDeltaTime);
+        private void MovePlayer()
+        {
+            var currentPosition = localTransform.position;
+            currentMovementInput = playerInput.Player.Move.ReadValue<Vector3>().normalized;
+            currentMovementInput.y = 0;
+
+            currentPosition += currentMovementInput * (MovementSpeedValue * Time.deltaTime);
+            transform.position = currentPosition;
         }
-        
+
         private void RotatePlayer()
-        {  
-            var rotateDirection = localRigidbody.velocity.normalized;
-            
-            if (rotateDirection != Vector3.zero) {
-                transform.forward = Vector3.Lerp(transform.forward, rotateDirection, RotationDumpingValue);
-            }
+        {
+            var movementDirection = currentMovementInput;
+
+            if (movementDirection == Vector3.zero) return;
+
+            var rotateDirection = Quaternion.LookRotation(movementDirection);
+            transform.rotation =
+                Quaternion.RotateTowards(transform.rotation, rotateDirection, RotationSpeedValue * Time.deltaTime);
+        }
+
+        private async void HandleKill()
+        {
+            mainModel.gameObject.SetActive(false);
+            await RunPlayableDirectorAsync(ghostDirector);
+        }
+
+        private UniTask RunPlayableDirectorAsync(PlayableDirector playableDirector)
+        {
+            playableDirector.Play();
+            return UniTask.WaitWhile(() => playableDirector.state == PlayState.Playing);
         }
     }
 }
